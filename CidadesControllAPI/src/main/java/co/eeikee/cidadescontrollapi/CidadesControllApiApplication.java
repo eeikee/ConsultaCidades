fd5e767a5f3daa2875312254821633fc6a29e7b7
Konuma Lima <rekm@gft.com>
2021-01-22 14:48:18 -0300

    created CidadesControllAPI

---
+++ CidadesControllAPI/src/main/java/co/eeikee/cidadescontrollapi/CidadesControllApiApplication.java
@@ -0,0 +1,13 @@
+package co.eeikee.cidadescontrollapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadesControllApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadesControllApiApplication.class, args);
+	}
+
+}
---
+++ CidadesControllAPI/src/test/java/co/eeikee/cidadescontrollapi/CidadesControllApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.eeikee.cidadescontrollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadesControllApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
